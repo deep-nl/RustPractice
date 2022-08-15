86bb318e5b73cbc3168ffb001e13ea0cc0696e72
倪磊 <nilei1797@gmail.com>
2022-08-15 10:47:45 +0800

    update

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // let condition = true;
+    // let number = if condition { 5 } else { 6 };
+
+    // println!("The value of number is: {number}");
+    // let s1 = String::from("hello");
+    let s1 = 6;
+    let s2 = s1;
+
+    println!("{}, world!", s1);
+}
